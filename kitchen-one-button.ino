e28f2a2f304768f98e8413787b4a3e42d9484ea6
Alexander Drach <alexander-drach@mail.ru>
2024-02-21 17:13:56 +0600

    enable() без удержания крайних кнопок

--- kitchen-one-button.ino
+++ kitchen-one-button.ino
@@ -9,8 +9,8 @@ GButton stopUpSensor(5);
 
 GButton hand(6);
 
-GButton moveUp(7);
-GButton moveDown(8);
+// GButton moveUp(7);
+// GButton moveDown(8);
 // GButton buttonSensor(12);
 
 // byte buttonSensor;
@@ -29,13 +29,15 @@ bool motorMovingDown = false;
 bool statePositionUp = false;
 bool statePositionDown = false;
 
+bool moving = false;
+
 void setup() {
   Serial.begin(9600);
 
   stepper.setRunMode(KEEP_SPEED);
   stepper.setMaxSpeed(1500);
 
-  // buttonSensor.setType(LOW_PULL);
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
@@ -48,8 +50,8 @@ void loop() {
 
   hand.tick();
 
-  moveUp.tick();
-  moveDown.tick();
+  // moveUp.tick();
+  // moveDown.tick();
 
   stepper.tick();
 
@@ -58,111 +60,95 @@ void loop() {
   if (hand.isPress()) {
     Serial.println("hand sunul ruku STOP"); // проверка на руку
     stepper.brake();
+    // stepper.autoPower(true); // снятие напряжения
     flag = false;
   }
 
-  // if (moveUp.isPress()) {
-  //   Serial.println("moveUp"); // движение фартука вверх
-  //   directionDown = false;
-
-  //   if (flag) {
-  //     stepper.stop();
-  //     flag = false;
-  //   } else {
-  //     stepper.setSpeed(speed);
-  //     flag = true;
-  //   }
-
-  //   if (stopUpSensor.isHold()) {
-  //     stepper.brake();
-  //     flag = false;
-  //     Serial.println("doehal vverh");
-  //   }   
-  // }
-
-  // if (moveDown.isPress()) { // движение фартука вниз
-  //   Serial.println("moveDown");
-  //   directionUp = false;
-    
-  //   if (flag) {
-  //     stepper.stop();
-  //     flag = false;
-  //   } else {
-  //     stepper.setSpeed(-speed);
-  //     flag = true;
-  //   };
-
-  //   if (stopDownSensor.isHold()) {
-  //     stepper.brake();
-  //     flag = false;
-  //     Serial.println("doehal vniz");
-  //   }
-  // }
-
   if (stopDownSensor.isPress()) {// остановка фартука внизу ДАТЧИК - 0
     stepper.brake();
+    digitalWrite(7, HIGH); // снятие напряжения без библиотеки
     flag = false;
     Serial.println("doehal vniz");
     statePositionDown = true;
     statePositionUp = false;
-  }
-
-  if (slowDownSpeed.isPress()) { // замедление скорости внизу ДАТЧИК - 1  
-    stepper.setSpeed(-200);
-    Serial.println("slowDownSpeed");
+    moving = false;
+    digitalWrite(7, HIGH);
   }  
-
-  if (slowUpSpeed.isPress()) { // замедление скорости вверху ДАТЧИК - 2
-    stepper.setSpeed(200);
-    Serial.println("slowUpSpeed");
-  }
 
   if (stopUpSensor.isPress()) { // остановка фартука вверху ДАТЧИК - 3
     stepper.brake();
+    digitalWrite(7, HIGH); // снятие напряжения без библиотеки
     flag = false;
     Serial.println("doehal vverh");
     statePositionUp = true;
     statePositionDown = false;
+    moving = false;
+    digitalWrite(7, HIGH);
   }
 
   if (motionDetected) {
-    Serial.println("moveUp"); // движение фартука вверх по сенсору
+    Serial.println("moveUp"); // движение фартука вверх по сенсору вверх/вниз
+
+    digitalWrite(7, LOW); 
 
     if (!firstMotionDetected) {
       stepper.setSpeed(speed);
       firstMotionDetected = true;
+      moving = true;
     }
 
     if (firstMotionDetected && statePositionDown) {
       stepper.setSpeed(speed);
+      moving = true;
     }
 
     if (firstMotionDetected && statePositionUp) {
       stepper.setSpeed(-speed);
+      moving = true;
     }
 
     if (stopUpSensor.isHold()) {
       stepper.brake();
+      digitalWrite(7, HIGH); // снятие напряжения без библиотеки
       flag = false;
       Serial.println("doehal vverh");
       statePositionUp = true;
       statePositionDown = false;
+      moving = false;
     }
 
     if (stopUpSensor.isHold() && motionDetected) {
       stepper.setSpeed(-speed);
+      moving = true;
     }
 
     if (stopDownSensor.isHold()) {
       stepper.brake();
+      digitalWrite(7, HIGH); // снятие напряжения без библиотеки
       flag = false;
       Serial.println("doehal vniz");
       statePositionDown = true;
       statePositionUp = false;
+      moving = false;
     }
 
     if (stopDownSensor.isHold() && motionDetected) {
       stepper.setSpeed(speed);
+      moving = true;
+    }
+  }
+
+  if (statePositionDown && moving) {
+    if (slowUpSpeed.isPress()) { // замедление скорости вверху ДАТЧИК - 2
+      stepper.setSpeed(200);
+      Serial.println("slowUpSpeed");
+    }
+  }
+
+  if (statePositionUp && moving) {
+    if (slowDownSpeed.isPress()) { // замедление скорости внизу ДАТЧИК - 1  
+      stepper.setSpeed(-200);
+      Serial.println("slowDownSpeed");
     }
   }
 }
